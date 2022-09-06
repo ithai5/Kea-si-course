aaae38a6ef81ab840614890a459f0a545842dab8
Ithai Gramse <ithai.g@ithais-mbp.lan>
2022-09-06 17:12:03 +0200

    resutl

--- Exercise_2/part_two/expressApi_server/app.js
+++ Exercise_2/part_two/expressApi_server/app.js
@@ -1,9 +1,15 @@
 import express from "express"
-
 const app = express()
 
 app.get("/timestamp", (req, res) => {
+    console.log("end point hitted");
     res.send({time: new Date().toISOString()})
 })
 
-app.listen(8080, () => console.log("Server on port", 8080))
+app.get("/fastApi/time",async(req,res)=> {
+    const response = await fetch("http://localhost:4200/timestamp")
+    const timeInPythonWorld = await response.json()
+    res.send(timeInPythonWorld)
+})
+
+app.listen(4200, () => console.log("Server on port", 4200))
--- Exercise_2/part_two/fastApi_server/main.py
+++ Exercise_2/part_two/fastApi_server/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from datetime import datetime, timezone
+import requests
+import json 
 
 app=FastAPI()
 
 @app.get("/timestamp")
 def date_authority():
     return {"time": datetime.now(timezone.utc).isoformat().replace("+00:00","Z")}
+
+@app.get("/express/time")
+async def date_in_express(): 
+    res =  requests.get("http://localhost:4200/timestamp")
+    result = json.loads(res.text)
+    return  {"message": result}
